6af54ec8622feba3c688f7ea080783402001ed5f
as <allensnape@qq.com>
2021-06-08 17:20:54 +0800

    +) 添加录音器

--- app/src/main/java/net/allape/noisefighter/MainActivity.java
+++ app/src/main/java/net/allape/noisefighter/MainActivity.java
@@ -29,7 +29,13 @@ import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.google.android.material.slider.Slider;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 import pub.devrel.easypermissions.EasyPermissions;
 
@@ -37,6 +43,12 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = "MainActivity";
     private static final int PERMISSION_REQUEST_CODE = 2021;
+
+    // 日期格式化
+    private static final SimpleDateFormat FORMAT = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss", Locale.CHINA);
+
+    // WAV文件头长度
+    private static final int WAV_HEADER_SIZE = 44;
 
     // OneShot震动
     private static final VibrationEffect ONE_SHOT = VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE);
@@ -47,6 +59,10 @@ public class MainActivity extends AppCompatActivity {
     private static final int CHANNEL = 1;
     // 码率 bytes per second
     private static final int BYTE_RATE = SAMPLE_RATE_IN_HZ * CHANNEL * 2;
+    // 音频采样大小 bit
+    private static final int SAMPLE_LENGTH = 16;
+    // 音频采样格式
+    private static final int SAMPLE_FORMAT = AudioFormat.ENCODING_PCM_16BIT;
 
     // 最大缓存数量, 超出时直接播放
     private static final int RECORDED_MAX_SIZE = 10000;
@@ -69,7 +85,11 @@ public class MainActivity extends AppCompatActivity {
     // 播放器
     private AudioTrack track;
 
-    private Button recordButton;
+    // 文件写入流, 不为null则表示需要写入文件
+    private RandomAccessFile wav;
+    // 已经写入了的数据长度(byte数量)
+    private long wavBytes = 0;
+
     private LineChart chart;
 
     @Override
@@ -99,6 +119,13 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         if (micRunnable != null) micRunnable.close();
         if (track != null) track.release();
+        if (wav != null) {
+            try {
+                wav.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
@@ -127,9 +154,97 @@ public class MainActivity extends AppCompatActivity {
         vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
         // 录音按钮
-        recordButton = findViewById(R.id.record_button);
+        final Button recordButton = findViewById(R.id.record_button);
         recordButton.setOnClickListener(view -> {
+            // 初始化写入功能
+            if (wav == null) {
+                try {
+                    String fileName = getExternalFilesDir(null) + "/" + FORMAT.format(new Date()) + ".wav";
+                    File wavFile = new File(fileName);
+                    if (!wavFile.exists()) {
+                        if (!wavFile.createNewFile()) {
+                            throw new IOException("failed to create wav file: " + fileName);
+                        }
+                    }
+                    wav = new RandomAccessFile(wavFile, "rw");
+                    wav.seek(WAV_HEADER_SIZE);
+                    Log.v(LOG_TAG, "start recording to file: " + fileName);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e(LOG_TAG, e.getMessage());
+                    Toast.makeText(this, "Failed to create WAV file", Toast.LENGTH_LONG).show();
+                }
+                recordButton.setText(R.string.StopRecording);
+            } else {
+                try {
+                    // 关闭并保存头部数据 https://zh.wikipedia.org/wiki/WAV
+                    byte[] header = new byte[WAV_HEADER_SIZE];
+                    // RIFF/WAVE header
+                    header[0] = 'R';
+                    header[1] = 'I';
+                    header[2] = 'F';
+                    header[3] = 'F';
+                    int totalBytesLength = (int) (wavBytes + 36);
+                    header[4] = (byte) (totalBytesLength & 0xff);
+                    header[5] = (byte) ((totalBytesLength >> 8) & 0xff);
+                    header[6] = (byte) ((totalBytesLength >> 16) & 0xff);
+                    header[7] = (byte) ((totalBytesLength >> 24) & 0xff);
+                    //WAVE
+                    header[8] = 'W';
+                    header[9] = 'A';
+                    header[10] = 'V';
+                    header[11] = 'E';
+                    // 'fmt ' chunk
+                    header[12] = 'f';
+                    header[13] = 'm';
+                    header[14] = 't';
+                    header[15] = ' ';
+                    // 4 bytes: size of 'fmt ' chunk
+                    header[16] = 16;
+                    header[17] = 0;
+                    header[18] = 0;
+                    header[19] = 0;
+                    // format = 1
+                    header[20] = 1;
+                    header[21] = 0;
+                    header[22] = (byte) CHANNEL;
+                    header[23] = 0;
+                    header[24] = (byte) (SAMPLE_RATE_IN_HZ & 0xff);
+                    header[25] = (byte) ((SAMPLE_RATE_IN_HZ >> 8) & 0xff);
+                    header[26] = (byte) ((SAMPLE_RATE_IN_HZ >> 16) & 0xff);
+                    header[27] = (byte) ((SAMPLE_RATE_IN_HZ >> 24) & 0xff);
+                    header[28] = (byte) (BYTE_RATE & 0xff);
+                    header[29] = (byte) ((BYTE_RATE >> 8) & 0xff);
+                    header[30] = (byte) ((BYTE_RATE >> 16) & 0xff);
+                    header[31] = (byte) ((BYTE_RATE >> 24) & 0xff);
+                    // block align
+                    header[32] = (byte) (CHANNEL * SAMPLE_LENGTH / 8);
+                    header[33] = 0;
+                    // bits per sample
+                    header[34] = SAMPLE_LENGTH;
+                    header[35] = 0;
+                    //data
+                    header[36] = 'd';
+                    header[37] = 'a';
+                    header[38] = 't';
+                    header[39] = 'a';
+                    header[40] = (byte) (wavBytes & 0xff);
+                    header[41] = (byte) ((wavBytes >> 8) & 0xff);
+                    header[42] = (byte) ((wavBytes >> 16) & 0xff);
+                    header[43] = (byte) ((wavBytes >> 24) & 0xff);
+                    wav.seek(0);
+                    wav.write(header);
 
+                    Log.v(LOG_TAG, "wav wrote header: " + WAV_HEADER_SIZE + "bytes");
+
+                    wav.close();
+                    wav = null;
+                    wavBytes = 0;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                recordButton.setText(R.string.StartRecording);
+            }
         });
 
         // 图表
@@ -207,6 +322,21 @@ public class MainActivity extends AppCompatActivity {
 
             // 重新渲染图表
             setData(values);
+
+            // 写入数据
+            if (wav != null) {
+                try {
+                    wav.write(data);
+                    wavBytes += data.length;
+                    Log.w(LOG_TAG, "wav wrote: " + data.length + "bytes, total: " + wavBytes + "bytes");
+                    if (wavBytes >= Integer.MAX_VALUE) {
+                        Log.w(LOG_TAG, "wav file overflow");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e(LOG_TAG, e.getMessage());
+                }
+            }
         });
         new Thread(micRunnable).start();
 
@@ -218,7 +348,7 @@ public class MainActivity extends AppCompatActivity {
                         .build())
                 .setAudioFormat(new AudioFormat.Builder()
                         .setChannelMask(AudioFormat.CHANNEL_OUT_MONO)
-                        .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                        .setEncoding(SAMPLE_FORMAT)
                         .setSampleRate(SAMPLE_RATE_IN_HZ)
                         .build())
                 .setBufferSizeInBytes(micRunnable.getBufferSize())
@@ -331,7 +461,7 @@ public class MainActivity extends AppCompatActivity {
             this.callback = callback;
 
             bufferSize = AudioRecord.getMinBufferSize(rateInHz,
-                    AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+                    AudioFormat.CHANNEL_IN_MONO, SAMPLE_FORMAT);
         }
 
         @Override
@@ -341,7 +471,7 @@ public class MainActivity extends AppCompatActivity {
 
             // Audio
             AudioRecord audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, rateInHz,
-                    AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, bufferSize);
+                    AudioFormat.CHANNEL_IN_MONO, SAMPLE_FORMAT, bufferSize);
 
             int bufferReadResult;
             byte[] audioData = new byte[bufferSize];
